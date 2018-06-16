8957bd86d757de8e7cf60c7476b735153f657155
Taran Pierce <taran.pierce@endurance.com>
2018-06-16 08:02:14 -0600

    added new iamges

---
+++ dist/photo-gallery.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <?php
+    $title = "Photo Gallery - Caddo Lake Bayou Tours";
+    $canonical = "/photo-gallery";
+    include("templates/_head.php");
+  ?>
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9HT2T6"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+   <div class="container body-content">
+    <?php include("templates/_navigation.php"); ?>
+    <section class="hero">
+      <div class="container">
+        <h1>Photo Gallery</h1>
+        <p>Here are some photos of Caddo Lake and the surrounding area so you can get an idea of what it's going to be like. You get the best views of the lake while out on the guided boat
+        tours so the views are spectacular.</p>
+        <p>Of course, like most things, it's even more beautiful in person. You don't have to take our word for it though, you can come and see for yourself. Call us today!</p>
+        <div class="image-gallery-container">
+          <div class="image-gallery">
+            <?php
+              $count = 30;
+
+              include("templates/gallery/_slide.php");
+            ?>
+          </div>
+          <div class="gallery-navigation clearfix">
+            <button class="btn btn-default prev pull-left">Previous</button>
+            <button class="btn btn-default next pull-right">Next</button>
+          </div>
+          <div class="modal fade" id="gallery-lightbox" tabindex="-1" role="dialog" aria-labelledby="gallery-lightbox">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                  <h4 class="modal-title" id="myModalLabel">Lightbox</h4>
+                </div>
+                <div class="modal-body"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <?php include("templates/_footer.php"); ?>
+   </div>
+   <script type="text/javascript" src="js/main.js"></script>
+  </body>
+</html>
---
+++ dist/templates/_head.php
@@ -0,0 +1,111 @@
+<?php
+  # set default page title if none set
+  if ($title === null) {
+    $title = "Caddo Lake Bayou Tours";
+  }
+
+  $base_url = "http://www.caddolakebayoutours.com";
+  # set default canonical
+  if ($canonical === null) {
+    $canonical = "/";
+  }
+
+  $url = $base_url . $canonical;
+?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php print_r( $title ); ?></title>
+  <meta name="description" content="Caddo Lake Bayou Tours">
+  <link rel="canonical" <?php print_r('href="' . $url . '"') ?> />
+  <meta name="author" content="Taran Pierce">
+  <meta name="description" content="Caddo Lake Bayou Tours offers a guided tour of the beautiful Caddo Lake at any time from sunrise to sunset. Whether you are looking for a guided fishing
+  tour, observe some of nature's wildlife, catch a jaw-dropping sunrise or sunset, take in some of the historic Caddo Lake culture or just enjoy some relaxing sight-seeing you are in for quite
+   a treat. Book a boat tour with Caddo Lake Bayou Tours and experience the wonder for yourself.">
+  <meta name="keywords" content="Caddo Lake boat tours, tours of Caddo Lake, guided fishing trip, fishing trip, fishing tour, fishing guide tours, river tours, Caddo Lake, caddo lake bayou tours, caddo lake tour guide, fishing guide, sunrise tours, sunset tours, swamp tours, caddo lake swamp, boat tours, personal tours, backwater, wildlife tours, gator, alligator, lake guide, caddo, lake, texas lake, natural lake, Louisiana lake, boats, bird watching, sightseeing, Caddo businesses, Texas, caddo lake information, uncertain texas, swamp tour, swamp adventure, caddo indians, spanish moss, fishing, caddo lake history, ">
+  <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107758647-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-107758647-2');
+  </script>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-N9HT2T6');</script>
+  <!-- End Google Tag Manager -->
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="preload stylesheet" href="css/base.css" as="style">
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+  <script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "LocalBusiness",
+    "image": [
+      "http://www.caddolakebayoutours.com/images/sunset/lake-sunset-xl.jpg"
+     ],
+    "@id": "http://www.caddolakebayoutours.com/",
+    "name": "Caddo Lake Bayou Tours",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "449 Cypress Drive",
+      "addressLocality": "Uncertain",
+      "addressRegion": " TX",
+      "postalCode": " 75661",
+      "addressCountry": "US"
+    },
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": 32.712023,
+      "longitude": -94.121835
+    },
+    "url": "http://www.caddolakebayoutours.com/",
+    "telephone": "903-570-2169",
+    "openingHoursSpecification": [
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday"
+        ],
+        "opens": "6:30",
+        "closes": "9:30"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Saturday",
+          "Sunday"
+        ],
+        "opens": "6:30",
+        "closes": "7:30"
+      }
+    ]
+  }
+  </script>
+</head>
