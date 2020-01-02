3e5c2473548ac4ce2a0349d1caf96fce7b90c22c
Taran Pierce <taran.pierce@endurance.com>
2020-01-02 09:42:20 -0500

    feature/refactoring-next-js
    started adding ability to pass lists into TextBlock

--- components/TextBlock.js
+++ components/TextBlock.js
@@ -3,13 +3,45 @@ import PropTypes from 'prop-types'
 import './textBlock.scss'
 
 const TextBlock = ( props ) => {
+  console.log('props.data.lists: ', props.data.lists)
   return (
     <div className={`text-wrapper`}>
       {props.data.titles.map( ( title, index ) => (
         <div key={index} dangerouslySetInnerHTML={{__html: title}} />
       ))}
+
       {props.data.paragraphs.map( ( paragraph, index) => (
         <p key={index} dangerouslySetInnerHTML={{__html: paragraph}} />
+      ))}
+      
+      {props.data.lists && props.data.lists.map( (list, index) => (
+        list.type === 'ul' ? (
+          <ul key={index}>
+            {list.items.map( (item, index) => (
+              <li key={index}>
+                {item.title && (
+                  <h4>{item.title}</h4>
+                )}
+                {item.paragraphs.map( (paragraph, index) => (
+                  <p>{paragraph}</p>
+                ))}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <ol key={index}>
+            {list.items.map( (item, index) => (
+              <li key={index}>
+                {item.title && (
+                  <h4>{item.title}</h4>
+                )}
+                {item.paragraphs.map( (paragraph, index) => (
+                  <p>{paragraph}</p>
+                ))}
+              </li>
+            ))}
+          </ol>
+        )
       ))}
     </div>
   )
--- pages/about.js
+++ pages/about.js
@@ -1,102 +1,87 @@
 import React from 'react'
-import Header from '../components/Header'
+import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import SideHug from '../components/SideHug'
-import Footer from '../components/Footer'
 
-const sideHug1 = {
+const sideHugData1 = {
   image: {
-    path: 'lake-cypress-trees.jpg',
+    path: 'lake-sunset.jpg',
+    alt: 'Caddo Lake, rich with history',
   },
   textData: [
     {
       titles: [
-        '<h1>About Caddo Lake Bayou Tours</h1>',
+        '<h3>About Caddo Lake Bayou Tours</h3>',
       ],
       paragraphs: [
         'We can take you out on a variety of tours on Caddo Lake so that you can relax and enjoy your time knowing you are in the hands of a well trained guide while on the lake.',
         'No matter what you are looking for, so long as you are wanting to have a good time, there are plenty of things to do on Caddo Lake.',
-      ]
-    },
-    {
-      titles: [
-        '<h3>Fishing Trips</h3>',
+        '<a class="btn btn-info" href="/contact">Contact us today</a>',
       ],
-      paragraphs: [
-        'There are plenty of great fishing spots across the lake. Come take a guided fishing trip for all your fishing needs.',
-      ]
-    },
-    {
-      titles: [
-        '<h3>Sightseeing</h3>',
-      ],
-      paragraphs: [
-        'Enjoy the beautiful lake, lots of wildlife and the overall mystique of Caddo Lake.',
-        'Visit historic Jefferson, Texas and enjoy all that it has to offer.',
-      ]
-    },
-    {
-      titles: [
-        '<h3>Sunrise and sunset tours</h3>',
-      ],
-      paragraphs: [
-        'Caddo Lake is always a beautiful sight to behold, but the sunrises and sunsets out on the lake are among some of the most gorgeous ones in the South.',
-      ]
-    },
-    {
-      titles: [
-        '<h3>and much more</h3>',
-      ],
-      paragraphs: [
-        'Contact us today to find out about the availability of tours and to check on seasonal prices.',
-        '<a class="btn" href="/contact">Contact us today</a>',
+      lists: [
+        {
+          'type': 'ul',
+          'items': [
+            {
+              'title': 'Fishing Trips',
+              'paragraphs': [
+                'There are plenty of great fishing spots across the lake. Come take a guided fishing trip for all your fishing needs.'
+              ]
+            },
+            {
+              'title': 'Sightseeing',
+              'paragraphs': [
+                'Enjoy the beautiful lake, lots of wildlife and the overall mystique of Caddo Lake.',
+                'Visit historic Jefferson, Texas and enjoy all that it has to offer.',
+              ]
+            },
+            {
+              'title': 'Sunrise and sunset tours',
+              'paragraphs': [
+                'Caddo Lake is always a beautiful sight to behold, but the sunrises and sunsets out on the lake are among some of the most gorgeous ones in the South.',
+                'Contact us today to find out about the availability of tours and to check on seasonal prices.',
+              ]
+            },
+          ]
+        }
       ]
     }
   ]
 }
 
-const sideHug2 = {
+const sideHugData2 = {
   image: {
-    path: 'lake-cypress-trees-2.jpg',
+    path: 'lake-spanish-moss.jpg',
+    alt: 'Caddo Lake, rich with history',
   },
-  imageSide: 'left',
   textData: [
     {
       titles: [
-        '<h2>About Caddo Lake</h2>',
+        '<h2>A lake rich with history</h2>',
       ],
       paragraphs: [
-        'Caddo Lake is steeped in history dating back to the early 1800\'s. The history which includes the Caddo Indian tribes that inhabited the area and the steamboat navigational trade from New Orleans, Louisiana to Jefferson, Texas are just some of the lake\'s rich history.',
-        'Check out some of our photos from around various spots of Caddo Lake.',
-        '<a class="btn" href="/photo-gallery">View our photos</a>',
-      ]
-    },
+        'Caddo Lake is steeped in history dating back to the early 1800\'s and one of the best ways to soak it in is with a guided boat tour. Sit back and relax while you take in the history which includes the Caddo Indian tribes that inhabited the area and the steamboat navigational trade from New Orleans to Jefferson, Texas are just some of the lakes rich history.',
+        'The names of certain areas of the lake such as Alligator Bayou, Starr Ditch, Ames Spring Basin, Potters Point, Britts Gap and Government Ditch among others give the lake a certain mystic created by the characters from it\'s past. It creates the perfect atomosphere for fishing, relaxing or watching the sunset.',
+      ],
+    }
+  ],
+  imageFirst: true,
+}
+
+const sideHugData3 = {
+  image: {
+    path: 'lake-spanish-moss.jpg',
+    alt: 'Caddo Lake, rich with history',
+  },
+  textData: [
     {
       titles: [
-        '<h3>Wildlife in the area</h3>',
+        '<h2>Photography Tours</h2>',
       ],
       paragraphs: [
-        'A recent study shows 216 types of birds, 90 different species of fish and reptiles and 47 different types of mammals.',
-        'Here are just a few examples of the wildlife you can expect to see:',
-      ]
-    },
-    {
-      titles: [
-        '<h3>Alligators</h3>',
+        'Looking to get some great pictures of the beautiful Bald Cypress trees covered in Spanish moss? Schedule a guided tour and go on an adventure to get all the images you need to complete your photography collection.',
+        'Whether you want to go on a wildlife photography tour with a group or with the peace and quiet of being alone to get those perfect shots, we can accommodate.',
       ],
-      paragraphs: [
-        'A member of the crocodile family, the American alligator is a living fossil from the Age of Reptiles, having survived on earth for 200 million years.',
-        '<a href="https://www.fws.gov/refuge/Caddo_Lake/wildlife/alligator.html" target="_blank" rel="noopener">More info here.</a>',
-      ]
-    },
-    {
-      titles: [
-        '<h3>Paddlefish</h3>',
-      ],
-      paragraphs: [
-        'The American paddlefish is a species of basal ray-finned fish closely related to sturgeons in the order Acipenseriformes.',
-        '<a href="https://www.fws.gov/refuge/caddo_lake/wildlife/paddlefish.html" target="_blank" rel="noopener">More info here.</a>',
-      ]
     }
   ]
 }
@@ -104,24 +89,20 @@ const sideHug2 = {
 class Page extends React.Component {
   constructor( props ) {
     super( props )
+  
+    this.state = {
+      activeTab: 'about',
+    }
   }
   
   render() {
     return (
-      <div className={`layout`}>
-        <div className={'main-container'}>
-          <Header 
-            pageTitle={'About | Caddo Lake Bayou Tours'} 
-            canonical={'/about'} 
-            activeTab={'about'}
-          />
-          <Hero 
-            imagePath={'lake-sunset-2.jpg'} />
-          <SideHug data={sideHug1} />
-          <SideHug data={sideHug2} />
-          <Footer />
-        </div>
-      </div>
+      <Layout pageTitle={`About - Caddo Lake Bayou Tours`} canonical={`/about/`}>
+        <Hero imagePath={`lake-sunset-2.jpg`} />
+        <SideHug data={sideHugData1} />
+        <SideHug data={sideHugData2} />
+        <SideHug data={sideHugData3} />
+      </Layout>
     )
   }
 }
