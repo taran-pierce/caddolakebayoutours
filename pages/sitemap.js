507b747bb83c0c7ba3fb24003ab76465b61b1ea7
Taran Pierce <taran.pierce@endurance.com>
2020-01-11 11:49:11 -0500

    feature/add-sitemap
    added sitemap back

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
+const links = [
+  {
+    "name": "About",
+    "href": "/about/"
+  },
+  {
+    "name": "Directions",
+    "href": "/directions/"
+  },
+  {
+    "name": "Things to do",
+    "href": "/things-to-do/"
+  },
+  {
+    "name": "Photo Gallery",
+    "href": "/photo-gallery/"
+  },
+  {
+    "name": "Contact",
+    "href": "/contact/"
+  }
+]
+
 class Page extends React.Component {
   constructor( props ) {
     super( props )
@@ -12,8 +35,23 @@ class Page extends React.Component {
   render() {
     return (
       <section>
-        <div className={`container`}>
+        <style jsx>{`
+          div {
+            padding: 1rem;
+            background: #fff;
+            border-left: 1px solid #cbcbcb;
+            border-right: 1px solid #cbcbcb;
+          }
+        `}</style>
+        <div>
           <h1>Sitemap</h1>
+          <ul>
+            {links.map( (link, index) => (
+              <li key={index}>
+                <a href={link.href}>{link.name}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
     )
