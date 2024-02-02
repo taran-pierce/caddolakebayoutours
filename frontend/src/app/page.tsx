1b3027b8604e191f9a6614b718ae66d66c81bcc2
Taran Pierce <taran.tpdesign@gmail.com>
2024-02-02 16:54:58 -0700

    feature/rework-site-in-typescript-nextjs
    fixing build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,94 @@
-import Head from 'next/head';
-import Hero from '../../src/components/Hero';
-import SplitContent from '../../src/components/SplitContent';
-import { getContent } from '../../src/utils/contentfulService.js';
+import Image from "next/image";
+import styles from "./page.module.css";
 
-export default function Page({
-  content,
-}: any) {
-  const {
-    splitContentSections,
-    hero,
-  } = content || null;
-
+export default function Home() {
   return (
-    <main>
-      <Head>
-        <title>{content.pageTitle}</title>
-        <link rel="canonical" href="https://www.caddolakebayoutours.com/" />
-      </Head>
-      {hero && (
-        <Hero
-          imagePath={hero.fields.imageName}
-          alt={hero.fields.imageAltText}
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
         />
-      )}
-      {splitContentSections && splitContentSections.map((splitContentSection: any) => (
-        <SplitContent
-          key={splitContentSection.sys.id}
-          contentfulData={splitContentSection}
-          imageFirst={splitContentSection?.fields?.imageFirst}
-        />
-      ))}
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
-    );
-};
-
-export async function getStaticProps() {
-  // ID for Contentful "Page" entry
-  const page = await getContent("76HzwUVnG9QIxWD3lK40Rt");
-  const content = page;
-
-  return { props: { content } }
+  );
 }
