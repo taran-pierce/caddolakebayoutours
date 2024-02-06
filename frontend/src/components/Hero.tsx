54d7f28fbab9f45f2739fe3df46c0cea9b169a52
Taran Pierce <taran.tpdesign@gmail.com>
2024-02-06 13:27:19 -0700

    feature/fix-hero-preload
    fix hero preload

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -3,22 +3,16 @@ import {
   useState,
   useRef,
 } from 'react';
-import Head from 'next/head';
-import {
-  CldImage,
-  getCldImageUrl,
-} from 'next-cloudinary';
+import { CldImage } from 'next-cloudinary';
 
 import styles from './hero.module.scss';
 
 export default function Hero({
   imagePath,
   alt,
-  heroImageData,
 }: {
   imagePath: string,
   alt: string,
-  heroImageData: any,
 }) {
   const [heroDimensions, setHeroDimensions] = useState({
     height: undefined,
@@ -26,10 +20,6 @@ export default function Hero({
   });
 
   const ref: any = useRef(undefined);
-
-  // filter to find the hero selected from Contentful
-  // will be an array of heros, need the matching one for this page
-  const currentHero = heroImageData && heroImageData.filter((hero: any) => hero.public_id === imagePath)[0];
 
   useEffect(() => {
     const { current }: any = ref;
@@ -45,27 +35,6 @@ export default function Hero({
       ref={ref}
       className={styles.hero}
     >
-      {currentHero && heroDimensions.height && (
-        <Head>
-          <link
-            rel="preload"
-            fetchpriority="high"
-            as="image"
-            // getCldImageUrl will fetch the matching URL with the transforms that is called below
-            // this way the correct image url can be preloaded since it is above the fold
-            href={getCldImageUrl({
-              width: heroDimensions.width,
-              height: heroDimensions.height,
-              src: currentHero.public_id,
-              quality: '50',
-              format: 'webp',
-              gravity: 'center',
-              crop: 'fill',
-            })}
-            type={`${currentHero.resource_type}/${currentHero.format}`}
-          />
-        </Head>
-      )}
       {heroDimensions.height && heroDimensions.width && (
         <CldImage
           width={heroDimensions.width}
