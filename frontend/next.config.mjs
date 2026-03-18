cecb3ddda689337c71269c0c74d6c74ff1503bba
Taran Pierce <taran.tpdesign@gmail.com>
2026-03-18 07:07:38 -0600

    Updates
    clean up comments

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,15 +7,6 @@ const withAnalyzer = withBundleAnalyzer({
 })
 
 const nextConfig = {
-  // TODO replace with directly in code:
-//   process.env.CONTENTFUL_ACCESS_TOKEN
-// process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
-// process.env.CONTENTFUL_ENVIRONMENT
-  // publicRuntimeConfig: {
-  //   CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-  //   CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  //   CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
-  // },
   output: 'export',
   images: {
     loader: 'custom',
